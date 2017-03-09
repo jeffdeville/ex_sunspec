bcaeaf9a0f8c2f1cfef219c08e04bc836def0537
Jeff Deville <jeffdeville@gmail.com>
2017-03-09 14:37:04 -0500

    fix fronius.exs cmd line test

--- fronius.exs
+++ fronius.exs
@@ -1,8 +1,11 @@
 defmodule Fronius do
+
+end
+defmodule Fronius do
   use ExSunspec, start: 40_001,
                  models: [1, 111, 120, 121, 122, 123],
                  model_1_length_65: true
-  field :model_type, :uint16, 216, 1, :rw, "Type of SunSpec models used for inverter and meter data. Write 1 or 2 and then immediately 6 to acknowledge setting.", "", %{1 => "Floating point", 2 => "Integer & SF"}
+  field :model_type, :uint16, 216, 1, :rw, "Type of SunSpec models used for inverter and meter data. Write 1 or 2 and then immediately 6 to acknowledge setting.", enum_map: %{1 => "Floating point", 2 => "Integer & SF"}
 
   def get_field(name) do
     field_defs()
